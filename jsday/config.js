54bada9562b39f80fb717d5fc8a334ccae7790a9
flrent <flrent@me.com>
2013-05-14 00:34:34 -0700

    Added production version for jsday

---
+++ jsday/config.js
@@ -0,0 +1,26 @@
+var globalConfig = {
+	name:"jsDay 2013",
+	uniqueId:"jsday",
+	hashtag:"#jsday",
+	infos: {
+		history:[
+	    	'<h2>History</h2>',
+	    	'<h3>THE MOST USED LANGUAGE</h3>',
+	    	'<h3>THE MOST NEEDED CONFERENCE</h3>',
+	    	"<p>As Douglas Crockford stated on his blog, JavaScript is “The World's Most Misunderstood Programming Language”, not to mention that nowadays javascript is everywhere and it is the most used programming language on Earth. </p>",
+	    	"<p>This is the rationale behind the “the most needed conference” tag line. From case studies to project management, from new technologies to established ones, from development environments to best practices and mobile development, everything a javascript ninja should know will be covered during the jsDay 2013.</p>",
+	    	"<p>Among the topics you will find client side and server side javascript, best-practices, success cases related to quality, revision control, test-driven development, continuous integration, jQuery, MooTools, YUI, NodeJS, Backbone.js, Three.js , SproutCore, Raphaël, Sencha Touch, jQuery Mobile, HTML5 and many more.</p>"
+	    ].join(""),
+	    howToCome:[
+            '<h2>Getting around Portland</h2>',
+            '<p>Hotel San Marco<br>Via Longhena, 42<br>37138 Verona — Italy</p>',
+        ].join(""),   
+	},
+	logo:"resources/images/logo.png",
+	tabs: {
+	  speakers:false,
+	  talks:true,
+	  favorites:true,
+  		infos:true
+    }
+};
