5ca148cac07ad153215e97ac6f1407c95fa81016
flrent <flrent@me.com>
2013-05-15 23:12:12 -0700

    Fixed mistake on data object

--- app/store/Favorites.js
+++ app/store/Favorites.js
@@ -8,7 +8,7 @@ Ext.define('Conference.store.Favorites', {
 		grouper: {
 		   groupFn: function(record) {
 		    	var dateStart = new Date(Date.parse(record.get("starting_at")));
-		   		return dateStart.getDate()+"th of "+Ext.Date.monthNames[dateStart.getMonth()+1]+ " - "+(dateStart.getHours().toString().length > 1 ? dateStart.getHours() : "0"+dateStart.getHours())+":"+(dateStart.getMinutes().toString().length > 1 ? dateStart.getMinutes() : "0"+dateStart.getMinutes());
+		   		return dateStart.getDate()+"th of "+Ext.Date.monthNames[dateStart.getMonth()]+ " - "+(dateStart.getHours().toString().length > 1 ? dateStart.getHours() : "0"+dateStart.getHours())+":"+(dateStart.getMinutes().toString().length > 1 ? dateStart.getMinutes() : "0"+dateStart.getMinutes());
 		   }
 		},
 		proxy: {
