6e33da4a3637a632034ec9ef39a685f4137a95cd
flrent <flrent@me.com>
2013-05-25 15:29:28 -0700

    Added version 1.0 for JSConf US 2013

--- jsconf/config.js
+++ jsconf/config.js
@@ -10,8 +10,9 @@ var globalConfig = {
 	    	'<p>Read more about <a href="http://2013.jsconf.us/a-jsconf-reflection">JSConf</a></p>'
 	    ].join(""),
 	    howToCome:[
-            '<h2>Getting to the conf</h2>',
-            '<p>Hotel San Marco<br>Via Longhena, 42<br>37138 Verona — Italy</p>',
+            '<h2>Getting here</h2>',
+            '<p>The Omni Amelia Island Plantation Resort is on Amelia Island in northeast Florida, just across the St. Marys River from Georgia and adjacent to the historic city of Fernandina. The resort overlooks the azure blue waters of the Atlantic Ocean on the east and the Intracoastal Waterway on the west. Though private, secluded and protected by a gated entry, it is relatively easy to get to.</p>',
+			'<p>It is just a short 45 minute drive from downtown Jacksonville, and 29 miles from Jacksonville International Airport. To make your journey to JSConf a little nicer, Nokia is sponsoring airport shuttles to and from Jacksonville International Airport (JAX) and the Omni Amelia Island for you. Please review the schedule below and ensure you understand how this will work before arriving at JAX as you might need to wait for a bus OR find other means of transportation based on your time of arrivals/departures. We have tried to use the information you provided us during activity registration to cover as many people as possible. If you have any questions, <a href="https://plus.google.com/communities/108690140179099971045">please post them to our G+ community</a>.</p>'
         ].join(""),   
 	},
 	logo:"resources/images/logo.png",
